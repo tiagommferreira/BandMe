67198748c38939f6d7f49b89cf036c231de19b4d
Tiago Ferreira <tiagommferreira55@gmail.com>
2015-04-25 13:54:06 +0100

    Changed from ActionBar to ToolBar to match Material Design standards

--- app/src/main/java/ui/band/me/MainActivity.java
+++ app/src/main/java/ui/band/me/MainActivity.java
@@ -2,16 +2,23 @@ package ui.band.me;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
     }
 
 
